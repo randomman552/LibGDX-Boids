2646cad3875b1a18ff7817034c822a6794cf8cba
George Grainger <GraingerGY@cardiff.ac.uk>
2021-05-16 16:07:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LibGDX-Boids
+ 
---
+++ desktop/src/com/randomman552/boids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.randomman552.boids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.randomman552.boids.Boids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Boids(), config);
+	}
+}
