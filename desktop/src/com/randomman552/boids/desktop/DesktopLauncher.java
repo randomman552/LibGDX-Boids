53e4d0a0911dbaf5e2312b914fd3044247998dc1
George Grainger <GraingerGY@cardiff.ac.uk>
2021-06-03 15:07:39 +0100

    Increase starting resolution

--- desktop/src/com/randomman552/boids/desktop/DesktopLauncher.java
+++ desktop/src/com/randomman552/boids/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.randomman552.boids.Boids;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new Boids(), config);
 	}
 }
